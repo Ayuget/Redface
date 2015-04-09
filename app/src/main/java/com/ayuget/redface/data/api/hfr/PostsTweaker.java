4d1eb441a75c9e09e50c3abdd0423fb262a4848e
Ayuget <ayuget@gmail.com>
2015-04-09 23:18:54 +0200

    Fixes #46 : classic-style quotes are now parsed and displayed correctly by the app

--- app/src/main/java/com/ayuget/redface/data/api/hfr/PostsTweaker.java
+++ app/src/main/java/com/ayuget/redface/data/api/hfr/PostsTweaker.java
@@ -39,7 +39,7 @@ public final class PostsTweaker implements Func1<List<Post>, List<Post>> {
 
     private static final String REGULAR_LINK_REGEX = "<a\\s*href=\"(http://forum\\.hardware\\.fr.*?)\"\\s*target=\"_blank\"\\s*class=\"cLink\">";
     private static final CallbackMatcher SMILEYS_REGEX = new CallbackMatcher("<img\\s*src=\"(http://forum\\-images\\.hardware\\.fr.*?)\"\\s*alt=\"(.*?)\".*?/>");
-    private static final CallbackMatcher QUOTES_AND_SPOILERS = new CallbackMatcher("(?:<div class=\\\"container\\\"><table class=\\\")(citation|spoiler)(?:[^>]+)(?:>)(?:.*?)(?:<b class=\\\")(s1|s1Topic)(?:\\\">)(?:(?:<a href=\")([^\\\"]+)(?:\")(?:[^>]+)(?:>))?", Pattern.DOTALL);
+    private static final CallbackMatcher QUOTES_AND_SPOILERS = new CallbackMatcher("(?:<div class=\\\"container\\\"><table class=\\\")(oldcitation|citation|spoiler)(?:[^>]+)(?:>)(?:.*?)(?:<b class=\\\")(s1|s1Topic)(?:\\\">)(?:(?:<a href=\")([^\\\"]+)(?:\")(?:[^>]+)(?:>))?", Pattern.DOTALL);
     private static final CallbackMatcher END_OF_QUOTES = new CallbackMatcher("(?:</td></tr></tbody></table>)", Pattern.DOTALL);
 
     @Inject MDEndpoints mdEndpoints;
@@ -58,7 +58,7 @@ public final class PostsTweaker implements Func1<List<Post>, List<Post>> {
             htmlContent = QUOTES_AND_SPOILERS.replaceAll(htmlContent, new CallbackMatcher.Callback() {
                 @Override
                 public String foundMatch(MatchResult matchResult) {
-                    boolean isQuote = matchResult.group(1).equals("citation");
+                    boolean isQuote = matchResult.group(1).equals("citation") || matchResult.group(1).equals("oldcitation");
                     String onClickEvent = isQuote ? "" : " onClick=\"toggleSpoiler(this)\"";
 
                     String output = "<div class=\"" + (isQuote ? "quote" : "spoiler") + "\"" + onClickEvent + "><b class=\"" + (isQuote ? "s1": "s1Topic") +"\">";
