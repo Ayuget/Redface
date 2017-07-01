9b7a0cd89084aec275656d994bb3898c4906d6d4
Ayuget <ayuget@gmail.com>
2017-07-01 17:55:37 +0200

    Better positioning with anchors instead of shitty algorithm
    May enable scroll anchoring feature in Chrome (this would be so useful
    in topics with a lot of images)

--- app/src/main/assets/templates/hfr.js
+++ app/src/main/assets/templates/hfr.js
@@ -9,7 +9,7 @@ function scrollToTop() {
 * Scrolls to the bottom of the page
 */
 function scrollToBottom() {
-    document.body.scrollTop = document.body.scrollHeight;
+    document.body.scrollTop = document.body.scrollHeight + 300;
 }
 
 /**
@@ -17,17 +17,7 @@ function scrollToBottom() {
 * @param id element id
 */
 function scrollToElement(id) {
-    var elem = document.getElementById(id);
-    var x = 0;
-    var y = 0;
-
-    while (elem != null) {
-        x += elem.offsetLeft;
-        y += elem.offsetTop;
-        elem = elem.offsetParent;
-    }
-
-    window.scrollTo(x, y);
+    location.hash = "#" + id;
 }
 
 /**
