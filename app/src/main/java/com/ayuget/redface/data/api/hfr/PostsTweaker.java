ec719863b355923c78171881d5387899904d3619
Ayuget <ayuget@gmail.com>
2017-11-29 22:59:45 +0100

    Fix #204 : internal https links are now properly handled by the app

--- app/src/main/java/com/ayuget/redface/data/api/hfr/PostsTweaker.java
+++ app/src/main/java/com/ayuget/redface/data/api/hfr/PostsTweaker.java
@@ -33,8 +33,8 @@ import javax.inject.Inject;
  * by the app, handles images cache, images download settings, ...
  */
 public final class PostsTweaker {
-    private static final String REGULAR_LINK_REGEX = "<a\\s*href=\"(http://forum\\.hardware\\.fr.*?)\"\\s*target=\"_blank\"\\s*class=\"cLink\">";
-    private static final CallbackMatcher SMILEYS_REGEX = new CallbackMatcher("<img\\s*src=\"(http://forum\\-images\\.hardware\\.fr.*?)\"\\s*alt=\"(.*?)\".*?/>");
+    private static final String REGULAR_LINK_REGEX = "<a\\s*href=\"(https|http)(://forum\\.hardware\\.fr.*?)\"\\s*target=\"_blank\"\\s*class=\"cLink\">";
+    private static final CallbackMatcher SMILEYS_REGEX = new CallbackMatcher("<img\\s*src=\"(https://forum\\-images\\.hardware\\.fr.*?)\"\\s*alt=\"(.*?)\".*?/>");
     private static final CallbackMatcher IMAGES_REGEX = new CallbackMatcher("<img\\s*src=\"https?://[^\"]*?\"\\s*alt=\"https?://[^\"]*?\"\\s*title=\"(https?://.*?)\".*?/>", Pattern.DOTALL);
     private static final CallbackMatcher QUOTES_AND_SPOILERS = new CallbackMatcher("(?:<div class=\\\"container\\\"><table class=\\\")(oldcitation|citation|spoiler)(?:[^>]+)(?:>)(?:.*?)(?:<b class=\\\")(s1|s1Topic)(?:\\\">)(?:(?:<a href=\")([^\\\"]+)(?:\")(?:[^>]+)(?:>))?(.+?:)?", Pattern.DOTALL);
     private static final CallbackMatcher END_OF_QUOTES = new CallbackMatcher("(?:</td></tr></tbody></table>)", Pattern.DOTALL);
@@ -52,7 +52,7 @@ public final class PostsTweaker {
             // Adds callbacks to directly handle all links (internal and external) within the app.
 
             // Regular links (within posts)
-            htmlContent = htmlContent.replaceAll(REGULAR_LINK_REGEX, "<a onclick=\"handleUrl(event, " + post.getId() + ", '$1');\" class=\"cLink\">");
+            htmlContent = htmlContent.replaceAll(REGULAR_LINK_REGEX, "<a onclick=\"handleUrl(event, " + post.getId() + ", '$1$2');\" class=\"cLink\">");
 
             // Simplify quotes HTML
             htmlContent = QUOTES_AND_SPOILERS.replaceAll(htmlContent, matchResult -> {
